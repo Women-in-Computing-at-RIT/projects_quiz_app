52d6823626d2bd317ff29b9a53baba2415a59be0
Mia Venesky <venesky.mia@gmail.com>
2025-11-04 19:13:45 -0500

    Added assets folder for images, added background image to main page, edited pubspec.yaml to add assets section for flutter dependencies

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -8,7 +8,18 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("PlaceHolder"),
+        // need to insert background image (add assets folder and edit pubspec.yaml
+        // TO DO: make assets directory and add to yaml
       ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/welcome_page_images/welcbckrnd.png',
+                ),
+                fit: BoxFit.cover
+            ),
+          ),
+        )
     );
   }
 }
