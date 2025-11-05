1fcf81eb0ef4158b637a9731fac47e6be7bc270c
seg7259 <seg7259@rit.edu>
2025-11-04 19:46:05 -0500

    heroes images list

--- lib/pages/heroes_page.dart
+++ lib/pages/heroes_page.dart
@@ -3,11 +3,23 @@ import 'package:flutter/material.dart';
 class HeroesPage extends StatelessWidget {
   const HeroesPage({super.key});
 
+  List<AssetImage> tanks = [AssetImage(assetName), ];
+  List<AssetImage> damage =[AssetImage(assetName), ];
+  List<AssetImage> support =[AssetImage(assetName), ];
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Heroes Page"),),
-      body: Text("Hello"),
+      //TODO: have different grid views for tanks, support, and damage
+      body: GridView.builder(
+          itemCount: 44,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          itemBuilder: (context, index)=>
+              Container(color:Colors.purple, margin: EdgeInsets.all(2),)),
     );
   }
 }
