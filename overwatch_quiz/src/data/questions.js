ee022982c8eb6a2b90e9c4d669b386d1f66050b3
raffertyc944 <cmr8387@g.rit.edu>
2026-04-14 20:04:04 -0400

    Done with Dp QS

--- overwatch_quiz/src/data/questions.js
+++ overwatch_quiz/src/data/questions.js
@@ -21,5 +21,38 @@ const questions = [
         {text: "Always someone else", value: 30}
     ]
   },
-
+  {
+    id: 3,
+    axis: "DP",
+    question: "If the enemy backline is exposed, how do you typically react?",
+    answers: [
+        {text: "I attack right away", value: 0},
+        {text: "I pressure them slightly", value: 10},
+        {text: "I wait for my teammate to shoot first", value: 20},
+        {text: "I hold my position and wait for my teammate to handle it", value: 30}
+    ]
+  },
+  {
+    id: 4,
+    axis: "DP",
+    question: "If something on your team is not working, what do you do?",
+    answers: [
+        {text: "I take control myself and fix it", value: 0},
+        {text: "I mention the issue to my teammates ", value: 10},
+        {text: "I wait for the team to adjust on their own", value: 20},
+        {text: "I adapt to the problem and continue playing ", value: 30}
+    ]
+  },
+  {
+    id: 5,
+    axis: "DP",
+    question: "What is your role as a teammate?",
+    answers: [
+        {text: "Tank", value: 0},
+        {text: "Offense", value: 10},
+        {text: "Defense", value: 20},
+        {text: "Support", value: 30}
+    ]
+  }
+]
 export default questions;
